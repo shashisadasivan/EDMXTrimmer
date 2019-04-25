ccedc4fbbebc4797cbd74474f56f9119a864c7a6
shashisadasivan <shashi.sadasivan@gmail.com>
2019-04-26 09:19:04 +1200

    EDMX Trimmer

---
+++ EDMXTrimmer/EDMXTrimmer/EdmxTrimmer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace EDMXTrimmer
+{
+    //TODO: add comments on methods and their purpose
+    class EdmxTrimmer
+    {
+        public string EdmxFile { get; private set; }
+        private XmlDocument _xmlDocument;
+        private const string PREFIXSTR = "-";
+
+        public EdmxTrimmer(string _edmxFile)
+        {
+            this.EdmxFile = _edmxFile;
+        }
+
+        public void Run()
+        {
+            this.LoadFile();
+            this.AnalyzeFile();
+        }
+
+        public void Trim(List<String> entitiesToKeep)
+        {
+            this.LoadFile();
+            this.AnalyzeFile(trim:true, entitiesToKeep:entitiesToKeep);
+        }
+
+        private void AnalyzeFile(string prefix = "", bool trim = false, List<String> entitiesToKeep = null)
+        {
+            foreach (XmlNode node in this._xmlDocument.DocumentElement.ChildNodes)
+            {
+                Console.WriteLine($"{node.Name}");
+                this.AnalyzeNode(node, PREFIXSTR, trim, entitiesToKeep);
+            }
+            if (trim == true)
+            {
+                this._xmlDocument.Save("Output.edmx");
+            }
+        }
+
+        private void AnalyzeNode(XmlNode node, string prefix = "", bool trim = false, List<String> entitiesToKeep = null)
+        {
+            List<XmlNode> nodesToRemove = new List<XmlNode>();
+
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                if (childNode.Name == "EntitySet" || childNode.Name == "EntityType")
+                {
+                    if (trim == true
+                        && entitiesToKeep != null
+                        && entitiesToKeep.Count > 0
+                        && entitiesToKeep.Contains(childNode.Attributes["Name"].Value, StringComparer.OrdinalIgnoreCase) == false)
+                    {
+                        // Delete this
+                        Console.WriteLine($"Deleting: {prefix}{childNode.Name}-{childNode.Attributes["Name"].Value}");
+                        //node.RemoveChild(childNode);
+                        nodesToRemove.Add(childNode);
+                    }
+                    else
+                    {
+                        //Console.WriteLine($"{prefix}{node.Name}");
+                        Console.WriteLine($"{prefix}{childNode.Name}-{childNode.Attributes["Name"].Value}");
+                        // Remove NavigationProperties here
+                        if (childNode.Name == "EntityType"
+                            && childNode.HasChildNodes == true)
+                        {
+                            foreach (XmlNode childNodeNavProp in childNode.ChildNodes)
+                            {
+                                if (childNodeNavProp.Name == "NavigationProperty")
+                                {
+                                    nodesToRemove.Add(childNodeNavProp);
+                                }
+                            }
+                        }
+                    }
+                }
+                else if(trim == true
+                        && childNode.Name == "Action")
+                {
+                    // delete all actions
+                    nodesToRemove.Add(childNode);
+                }
+                else if(childNode.HasChildNodes)
+                {
+                    this.AnalyzeNode(childNode, prefix + PREFIXSTR, trim, entitiesToKeep);
+                }
+            }
+
+            if(nodesToRemove.Count > 0)
+            {
+                foreach (var nodeToRemove in nodesToRemove)
+                {
+                    XmlNode parentNode = nodeToRemove.ParentNode;
+                    parentNode.RemoveChild(nodeToRemove);
+                }
+            }
+        }
+
+        private void LoadFile()
+        {
+            this._xmlDocument = new XmlDocument();
+            this._xmlDocument.Load(this.EdmxFile);
+        }
+    }
+}
---
+++ EDMXTrimmer/EDMXTrimmer/Program.cs
@@ -0,0 +1,57 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EDMXTrimmer
+{
+    public class Options
+    {
+        [Option(
+            Required = true,
+            HelpText = "EDMX source file")]
+        public string EdmxFile { get; set; }
+
+        [Option(
+            Required = false,
+            HelpText = "Enter the public name & collection name. All values to be sepearted with commas",
+            Separator = ',')]
+        public IEnumerable<string> EntitiesToKeep { get; set; }
+
+        //[Option(
+        //    Required = false,
+        //    HelpText = "If trimming then set name of file, otherwise will create default in executing directory")]
+        //public string OutputFileName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string edmxFile = @"C:\temp\testedmx.edmx";
+            
+            EdmxTrimmer trimmer = new EdmxTrimmer(edmxFile);
+            //trimmer.Run();
+
+            List<string> entitiesToKeep = new List<string>() { "CustomersV3", "CustomerV3" };
+            trimmer.Trim(entitiesToKeep);
+            */
+            
+            Options opt = new Options();
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => opt = opts);
+
+            EdmxTrimmer trimmer = new EdmxTrimmer(opt.EdmxFile);
+            if (opt.EntitiesToKeep.Count() > 0)
+            {
+                //TODO: use the output file name
+                trimmer.Trim(opt.EntitiesToKeep.ToList());
+            }
+            else
+            {
+                trimmer.Run();
+            }
+            
+        }
+    }
+}
