79d0c41ec573787ec9368971f77edee329ebd450
shashisadasivan <shashi.sadasivan@gmail.com>
2019-05-03 16:19:42 +1200

    Revert "Revert "Removed requirement to have the singular entity name""
    This reverts commit 2c88218c1cded472e786dc383e20b4e58abae052.

--- EDMXTrimmer/EDMXTrimmer/EdmxTrimmer.cs
+++ EDMXTrimmer/EDMXTrimmer/EdmxTrimmer.cs
@@ -9,19 +9,24 @@ namespace EDMXTrimmer
     //TODO: add comments on methods and their purpose
     class EdmxTrimmer
     {
+        private const string PREFIXSTR = "-";
         public string EdmxFile { get; private set; }
         private XmlDocument _xmlDocument;
-        private const string PREFIXSTR = "-";
+        private bool _verbose;
+        
+        public List<string> EntitiesToKeep { get; private set; }
 
-        public EdmxTrimmer(string _edmxFile)
+        public EdmxTrimmer(string edmxFile, bool verbose = false )
         {
-            this.EdmxFile = _edmxFile;
+            this.EdmxFile = edmxFile;
+            this._verbose = verbose;
         }
 
         public void Run()
         {
+            //this.EntitiesToKeep = new List<string>();
             this.LoadFile();
-            this.AnalyzeFile();
+            this.AnalyzeFile(trim:false, entitiesToKeep:EntitiesToKeep);
         }
 
         public void Trim(List<String> entitiesToKeep)
@@ -30,11 +35,22 @@ namespace EDMXTrimmer
             this.AnalyzeFile(trim:true, entitiesToKeep:entitiesToKeep);
         }
 
+        public void AnalyzeAndTrim(List<String> entitiesToKeep)
+        {
+            this.EntitiesToKeep = new List<string>();
+            this.EntitiesToKeep.AddRange(entitiesToKeep);
+            this.Run();
+            this.Trim(this.EntitiesToKeep);
+        }
+
         private void AnalyzeFile(string prefix = "", bool trim = false, List<String> entitiesToKeep = null)
         {
             foreach (XmlNode node in this._xmlDocument.DocumentElement.ChildNodes)
             {
-                Console.WriteLine($"{node.Name}");
+                if (this._verbose)
+                {
+                    Console.WriteLine($"{node.Name}");
+                }
                 this.AnalyzeNode(node, PREFIXSTR, trim, entitiesToKeep);
             }
             if (trim == true)
@@ -46,7 +62,10 @@ namespace EDMXTrimmer
         private void AnalyzeNode(XmlNode node, string prefix = "", bool trim = false, List<String> entitiesToKeep = null)
         {
             List<XmlNode> nodesToRemove = new List<XmlNode>();
-
+            if(entitiesToKeep == null)
+            {
+                entitiesToKeep = new List<string>();
+            }
             foreach (XmlNode childNode in node.ChildNodes)
             {
                 if (childNode.Name == "EntitySet" || childNode.Name == "EntityType")
@@ -57,16 +76,18 @@ namespace EDMXTrimmer
                         && entitiesToKeep.Contains(childNode.Attributes["Name"].Value, StringComparer.OrdinalIgnoreCase) == false)
                     {
                         // Delete this
-                        Console.WriteLine($"Deleting: {prefix}{childNode.Name}-{childNode.Attributes["Name"].Value}");
+                        if (this._verbose)
+                        {
+                            Console.WriteLine($"Deleting: {prefix}{childNode.Name}-{childNode.Attributes["Name"].Value}");
+                        }
                         //node.RemoveChild(childNode);
                         nodesToRemove.Add(childNode);
                     }
                     else
                     {
-                        //Console.WriteLine($"{prefix}{node.Name}");
-                        Console.WriteLine($"{prefix}{childNode.Name}-{childNode.Attributes["Name"].Value}");
                         // Remove NavigationProperties here
-                        if (childNode.Name == "EntityType"
+                        if (trim == true
+                            && childNode.Name == "EntityType"
                             && childNode.HasChildNodes == true)
                         {
                             foreach (XmlNode childNodeNavProp in childNode.ChildNodes)
@@ -77,15 +98,37 @@ namespace EDMXTrimmer
                                 }
                             }
                         }
+
+                        if (trim == false
+                            && childNode.Name == "EntitySet"
+                            && entitiesToKeep.Contains(childNode.Attributes["Name"].Value, StringComparer.OrdinalIgnoreCase) == true)
+                        {
+                            // If this is the entitySet, extract the entityType here
+                            var entityType = childNode.Attributes["EntityType"].Value;
+                            // this is stored as Microsoft.Dynamics.DataEntities.CustomerV3, we only want the name CustomerV3
+                            entityType = entityType.Replace("Microsoft.Dynamics.DataEntities.", "");
+                            this.EntitiesToKeep.Add(entityType);
+                            if (this._verbose)
+                            {
+                                Console.WriteLine($"Found entityType {entityType} from Entity {childNode.Attributes["Name"].Value}");
+                            }
+                            //Console.WriteLine($"{prefix}{node.Name}");
+                            if (this._verbose)
+                            {
+                                Console.WriteLine($"{prefix}{childNode.Name}-{childNode.Attributes["Name"].Value}");
+                            }
+                        }
                     }
                 }
-                else if(trim == true
+                else if (trim == true
                         && childNode.Name == "Action")
                 {
                     // delete all actions
                     nodesToRemove.Add(childNode);
                 }
-                else if(childNode.HasChildNodes)
+
+                if (!(childNode.Name == "EntitySet" || childNode.Name == "EntityType")
+                    && childNode.HasChildNodes)
                 {
                     this.AnalyzeNode(childNode, prefix + PREFIXSTR, trim, entitiesToKeep);
                 }
